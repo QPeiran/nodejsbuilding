41b9073782cbf6fa6837662bd21c50bd5ba39239
QPeiran <quanpeiran@gmail.com>
2019-10-13 22:54:58 +1300

    commiting 1

--- myApp.js
+++ myApp.js
@@ -1,1 +1,10 @@
-console.log("greetings from Peiran");
+var time = 0;
+
+var timer =
+setInterval (function() {
+    time += 2;
+    console.log("greetings from Peiran" + time);
+    if (time > 5) {
+        clearInterval(timer);
+    }
+}, 2000)
