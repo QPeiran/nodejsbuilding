ee64db811e6cfc7884abac8ed2b22001f81859df
Peiran Quan <quanpeiran@gmail.com>
2019-11-13 16:36:51 +1300

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,10 +1,30 @@
+//////////////////////////////////////////////////////////////section 1: setInterval
 var time = 0;
+var timer = setInterval(() => {
+  time += 2;
+  console.log('Greeeetings!' + time + "sec");
+  if (time >= 5){
+    clearInterval(timer);
+  }
+},2000);
+////////////////////////////////////////////////////////////////section 2:
+console.log(__dirname);
+console.log(__filename);
+//////////////////////////////////////////////////////////////section 3: function chain
+var sayBye = function(x){
+  console.log('bye ' + x);
+};
+sayBye("Kunkka");
 
-var timer =
-setInterval (function() {
-    time += 2;
-    console.log("greetings from Peiran" + time);//
-    if (time > 5) {
-        clearInterval(timer);
-    }
-}, 2000)
+function callBye(fun){
+  fun("SF");
+}
+callBye(sayBye);
+/////////////////////////////////////////////////////////////section 4: module and requir()
+
+const MyCount = require('./count');;
+console.log(MyCount.counter(['Red','Green','Blue']));
+console.log(MyCount.adder(5,9));
+console.log(MyCount.adder(5, MyCount.pi));
+
+//////////////////////////////////////////////////////section 5:
