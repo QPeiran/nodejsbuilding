368f45c4658b9899217bffadbfa305442966c36a
QPeiran <quanpeiran@gmail.com>
2019-11-14 15:07:55 +1300

    part1 finished

--- part1.js
+++ part1.js
@@ -1,4 +1,4 @@
-/*
+/* */
 //////////////////////////////////////////////////////////////section 1: setInterval
 var time = 0;
 var timer = setInterval(() => {
@@ -27,8 +27,36 @@ const MyCount = require('./count');;
 console.log(MyCount.counter(['Red','Green','Blue']));
 console.log(MyCount.adder(5,9));
 console.log(MyCount.adder(5, MyCount.pi));
-*/
-//////////////////////////////////////////////////////section 5: file system or module 'fs'
+
+//////////////////////////////////////////////////////section 5:
+const events = require('events');
+const util = require('util');
+
+const myEvent = new events();
+myEvent.on('Trigger', (msg) => {console.log(`The trigger is calling you ${msg}`);})
+myEvent.emit('Trigger',"badass");
+
+function MyStream() {
+    this.streamName = "Yolo";
+}
+
+util.inherits(MyStream, events.EventEmitter);
+
+
+MyStream.prototype.write = function(data) {
+    this.emit("data", data);
+}
+var stream = new MyStream();
+
+//console.log(stream instanceof events.EventEmitter);
+//console.log(MyStream.super_ === events.EventEmitter);
+//console.log(stream.streamName);
+stream.on("data", function(data) {
+    console.log('Received data: "' + data + '"');
+})
+stream.write("It works!");
+
+//////////////////////////////////////////////////////section 6: file system or module 'fs'
 var fs = require('fs');
 
 fs.readFile('README.md', 'utf8', function(err, data){
@@ -38,3 +66,4 @@ fs.readFile('README.md', 'utf8', function(err, data){
     console.log('Replaced!');
   });
 });
+//both readFile and writeFile need a call back function!*/
