288f4500726312d53fa8603cca1df260029ef776
QPeiran <quanpeiran@gmail.com>
2019-11-22 17:24:39 +1300

    rounting to serve differnt contents

--- part3.js
+++ part3.js
@@ -1,11 +1,25 @@
+////////////////////////secton 8: routing to serve diffent contents
 const fs = require('fs');
 const http = require('http');
 
 var server = http.createServer();
-server.on('request',(req,res) => {
+server.on('request', (req,res) => {
   console.log('request was made: ' + req.url);
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  fs.createReadStream(__dirname + '/index.html', 'utf8')
-  .pipe(res);
+  if (req.url === '/' || req.url === '/home')
+  {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/index.html', 'utf8')
+    .pipe(res);
+  }
+  else if (req.url === '/api')
+  {
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      fs.promises.readFile(__dirname + '/ExampleJSON.json', 'utf8')
+      .then((data) => {
+        let pdl = JSON.parse(data);
+        res.write(pdl.name + " is " + pdl.age + " years old");
+      });
+      //.then()
+  }
 });
 server.listen(8080, '127.0.0.1');
