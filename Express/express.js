6af935c1ca9f504a2bcbeaa8a3ed8806c9eec1de
QPeiran <quanpeiran@gmail.com>
2019-12-05 15:30:45 +1300

    ejsTemplate

--- Express/express.js
+++ Express/express.js
@@ -4,16 +4,18 @@
 
  const app = express();
 
+ app.set('view engine', 'ejs');
+
   app.get('/', function(req, res){
     res.sendFile('index.html', {'root': __dirname + '/../'}); // level up the path(to its parent folder)
   });
 
-  app.get('/contact', function(req, res){
-    res.send('this is the contact page');
+  app.get('/readme', function(req, res){
+    res.sendFile('README.MD', {'root': __dirname + '/../'});
   });
 
-  app.get('/profile/:id', function(req, res){
-    res.send('this is the id: ' + req.params.id);
+  app.get('/profile/:name', function(req, res){
+    res.render('viewTemplate', {person: req.params.name});
   });
 
 
