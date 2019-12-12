25f8ae66fb11a8fe6b8592747e7890e2903e690f
QPeiran <quanpeiran@gmail.com>
2019-12-12 17:26:38 +1300

    POST contact (no Success Handler yet!)

--- Express/express.js
+++ Express/express.js
@@ -1,6 +1,7 @@
 //an example to script serverside with express
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -10,16 +11,21 @@ app.set('view engine', 'ejs');
   next();
 })*/
 app.use('/Style', express.static('Style'));
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-  app.get('/', function(req, res){
+app.get('/', function(req, res){
     res.sendFile('index.html',  {'root': __dirname + '/../'}); // level up the path(to its parent folder)
   });
-
+//////////////////////////////////////////////////////////////////////same route but different methods
   app.get('/contact', function(req, res){
     res.render('contactTemplate');
   });
-
-  app.get('/profile/:name', function(req, res){
+  app.post('/contact', urlencodedParser, (req,res) => {
+    console.log(req.body);
+    //res.render('contactSuccessTemplate');
+  });
+/////////////////////////////////////////////////////////////////////
+app.get('/profile/:name', function(req, res){
     var data = {age: 40, title: 'Manager', car: ['Wu', 'Ling', 'Hong', 'Guang']};
     res.render('viewTemplate', {person: req.params.name, info: data});
   });
